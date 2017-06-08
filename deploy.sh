f7db964329cab81c9ea005e0c692a1fd4ceeb1a9
Romain Marecat <romain.marecat@gmail.com>
2017-06-08 09:56:34 +0200

    Added robots.txt

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,6 @@
 ng build -aot -prod --base-href "https://romainmarecat.github.io/ng2-todo-miashs/"
 cp dist/index.html dist/404.html
 cp src/sitemap.xml dist/sitemap.xml
+cp src/robots.txt dist/robots.txt
 ngh
 exit 0
